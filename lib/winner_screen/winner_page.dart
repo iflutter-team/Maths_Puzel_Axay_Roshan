1e39e492945b35d4bee92115b394311958534019
Roshan Paladiya <roshanpaladiya2004@gmail.com>
2023-06-20 19:15:45 +0530

    last

--- lib/winner_screen/winner_page.dart
+++ lib/winner_screen/winner_page.dart
@@ -1,4 +1,3 @@
-import 'package:demo_math_puzzel/data_screen/data_screen.dart';
 import 'package:demo_math_puzzel/winner_screen/winner_controller.dart';
 import 'package:demo_math_puzzel/winner_screen/winner_widget.dart';
 import 'package:flutter/material.dart';
@@ -10,7 +9,6 @@ class WinPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Get.put(WinnerPageController());
-    DataPage(context);
     return Scaffold(
       body: SafeArea(
         child: winnerPageWidget(),
